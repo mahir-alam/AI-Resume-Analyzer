7e51a0db3fc92a32617d2aa1ce59540672eafbc3
30187962 <mahir.alam@ucalgary.ca>
2026-04-02 06:26:32 -0600

    feat: add landing page route as public homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
@@ -10,22 +11,23 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={<Navigate to={token ? "/dashboard" : "/login"} />}
-        />
+        <Route path="/" element={<Home />} />
+
         <Route
           path="/login"
           element={token ? <Navigate to="/dashboard" /> : <Login />}
         />
+
         <Route
           path="/signup"
           element={token ? <Navigate to="/dashboard" /> : <Signup />}
         />
+
         <Route
           path="/dashboard"
           element={token ? <Dashboard /> : <Navigate to="/login" />}
         />
+
         <Route
           path="/history"
           element={token ? <History /> : <Navigate to="/login" />}
